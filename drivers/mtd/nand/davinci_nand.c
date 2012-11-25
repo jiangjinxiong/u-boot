15689d1ce5a9e2f34fb121db9f8ce38ee2731a85
jiangjinxiong <jiangjinxiong@tongfangcloud.com>
2012-11-25 23:10:48 +0800

    修改了nand_ecclayout，使其与dm36x启动采用的ecc一致

--- drivers/mtd/nand/davinci_nand.c
+++ drivers/mtd/nand/davinci_nand.c
@@ -277,6 +277,7 @@ static int nand_davinci_correct_data(struct mtd_info *mtd, u_char *dat,
 #ifdef CONFIG_SYS_NAND_4BIT_HW_ECC_OOBFIRST
 static struct nand_ecclayout nand_davinci_4bit_layout_oobfirst = {
 #if defined(CONFIG_SYS_NAND_PAGE_2K)
+/*
 	.eccbytes = 40,
 	.eccpos = {
 		24, 25, 26, 27, 28,
@@ -287,7 +288,16 @@ static struct nand_ecclayout nand_davinci_4bit_layout_oobfirst = {
 		},
 	.oobfree = {
 		{.offset = 2, .length = 22, },
-	},
+*/
+	.eccbytes = 40,
+	.eccpos = {
+		6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
+		22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 
+		38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 
+		54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
+		},
+	.oobfree = {{2, 4}, {16, 6}, {32, 6}, {48, 6}},
+
 #elif defined(CONFIG_SYS_NAND_PAGE_4K)
 	.eccbytes = 80,
 	.eccpos = {
