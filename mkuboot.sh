557cca44292ecef4b98a63ab77156d3df34c94e2
jiangjinxiong <348248135@qq.com>
2014-02-22 18:43:18 +0800

    修改拷贝路径

--- mkuboot.sh
+++ mkuboot.sh
@@ -4,6 +4,6 @@ export BUILD_DIR="tmp/build"
 export CROSS_COMPILE=arm-none-linux-gnueabi-
 make distclean
 make davinci_dm365evm_config
-make all
-echo "cp $BUILD_DIR/u-boot.bin $sdboot_dir/sdcard_flash/u-boot_dm36x_sdmmc.bin"
-cp $BUILD_DIR/u-boot.bin $sdboot_dir/sdcard_flash/u-boot_dm36x_sdmmc.bin
+make
+echo "cp $BUILD_DIR/u-boot.bin $sd_dm36x_dir/sdcard_flash/u-boot_dm36x_sdmmc.bin"
+cp $BUILD_DIR/u-boot.bin $sd_dm36x_dir/sdcard_flash/u-boot_dm36x_sdmmc.bin
